60eb7527f6a89db670c5119c4d7b3b74dc7beae0
Chaz-Keck <cmkeck@ucsd.edu>
2017-05-12 15:28:53 -0700

    fixed regfile

--- Lab2/reg_file/reg_file_ABC.sv
+++ Lab2/reg_file/reg_file_ABC.sv
@@ -1,8 +1,8 @@
 // Create Date:    2017.01.25
-// Design Name: 
-// Module Name:    reg_file 
+// Design Name:
+// Module Name:    reg_file_ABC
 //
-// Additional Comments: 					  $clog2
+// Additional Comments:
 
 module reg_file #(parameter W=8, D=4)(
   input           clk,
@@ -10,20 +10,21 @@ module reg_file #(parameter W=8, D=4)(
   input  [ D-1:0] raddrB,
                   waddr,
   input  [ W-1:0] data_in,
-  output [ W-1:0] data_outA,
-  output logic [W-1:0] data_outB
+  output logic [W-1:0] data_outA, data_outB
     );
 
-// W bits wide [W-1:0] and 2**4 registers deep 	 
+// W bits wide [W-1:0] and 2**4 registers deep
 logic [W-1:0] registers[2**D];	  // or just registers[16] if we know D=4 always
 
-// combinational reads w/ blanking of address 0
-assign      data_outA = registers[0];
-always_comb data_outB = registers[raddrB];
+// combinational reads w/ data_outA always reading from register 0
+always_comb begin
+      data_outA = registers[0];
+      data_outB = registers[raddrB];
+end
 
 // sequential (clocked) writes
-always_ff @ (posedge clk)
-  if (write_en && waddr)
+always_ff @ (posedge clk)begin
+  if (write_en)
     registers[waddr] <= data_in;
-
+end
 endmodule
---
+++ Lab2/reg_file/reg_file_tb.sv
@@ -0,0 +1,78 @@
+// Company:
+// Engineer:
+//
+// Create Date:   13:31:49 10/27/2011
+// Design Name:   reg_file
+// Module Name:   reg_file_tb.v
+// Project Name:  lab_basics
+// Target Device:
+// Tool versions:
+// Description:
+//
+// Verilog Test Fixture created by ISE for module: reg_file
+//
+// Dependencies:
+//
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+//
+////////////////////////////////////////////////////////
+
+module reg_file_tb;
+	parameter DT = 4,
+	          WT = 8;
+// DUT Input Drivers
+	bit           clk;	      // bit can be only 0, 1 (no x or z)
+  bit           write_en;   // bit self-initializes to 0, not x (handy)
+	bit [ DT-1:0] raddrB,
+	              waddr;
+	bit [ WT-1:0] data_in;
+
+// DUT Outputs
+	wire [WT-1:0] data_outA,
+                data_outB;
+
+// Instantiate the Unit Under Test (UUT)
+	reg_file #(.W(WT),.D(DT)) uut(
+	  .clk        ,     // .CLK(CLK),
+	  .write_en   ,
+	  .raddrB     ,
+	  .waddr      ,
+	  .data_in    ,
+	  .data_outA  ,
+	  .data_outB
+	);
+
+	initial begin
+// Initialize Inputs done for us by "bit"
+
+// Wait 100 ns for global reset to finish
+	  #100ns;
+
+// Add stimulus here
+// check if writing works
+		raddrB     =  'h1;
+	  waddr      =  'h1;
+	  data_in    =  'hCD;
+	  write_en   =  1;
+
+	  #60ns;
+//verify writing without RegWrite has no impact
+	  write_en   =  0;
+	  waddr      =  'h2;
+	  data_in    = 'h12;
+	  raddrB     =  'h2;
+	  #40ns;
+
+//verify raddrA is always r0
+		write_en   = 1;
+		waddr      = 'h0;
+		data_in    = 'h23;
+		#40ns $stop;
+	end
+always begin
+  #10ns clk = 1;
+  #10ns clk = 0;
+end
+endmodule
